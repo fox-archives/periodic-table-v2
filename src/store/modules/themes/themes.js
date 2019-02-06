00c552c98c42aea53814f74f7d37fc1dfce054cd
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-06 10:14:03 -0800

    Create theme module in Vuex store

---
+++ src/store/modules/themes/themes.js
@@ -0,0 +1,23 @@
+const state = {
+  // For now, add lightTheme in currentTheme. Do replacing, etc. later
+  currentTheme: {
+    name: "Light Theme"
+  },
+  lightTheme: {},
+  darkTheme: {},
+  outlineTheme: {}
+};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import elementData from "@/store/modules/element-data/elementData";
+import themes from "@/store/modules/themes/themes.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    elementData
+    elementData,
+    themes
   }
-  // state: {},
-  // mutations: {},
-  // actions: {}
 });
