5a105338f412afdee2bcc71dbba61d93f0fba1df
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-11 16:20:52 -0800

    Syntax continuity

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const outDir = "back/public/json";
 
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-task-steps-per-folder.md
 let getFolders = dir => fs.readdirSync(dir).filter(file => {
-  return fs.statSync(path.join(dir, file)).isDirectory()
+  return fs.statSync(path.join(dir, file)).isDirectory();
 });
 
 gulp.task("move-wolf-json", done => {
