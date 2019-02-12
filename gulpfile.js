8ea051a1772bf50087388fc24ee0bb0119a150bb
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-11 16:18:50 -0800

    Add gulp task to move JSON JSON moves from the wolf folder to the public folder of the backend server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require("gulp");
+const fs = require("fs");
+const path = require("path");
+const outDir = "back/public/json";
 
-exports.default = defaultTask
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-task-steps-per-folder.md
+let getFolders = dir => fs.readdirSync(dir).filter(file => {
+  return fs.statSync(path.join(dir, file)).isDirectory()
+});
+
+gulp.task("move-wolf-json", done => {
+  let dirs = getFolders("wolf");
+
+  // Why doesn't path.join seem to be effective
+  dirs.map(folder => {
+    console.log(path.join("wolf", folder + "/*.json"));
+    return gulp.src("wolf/" + folder + "/*.json")
+      .pipe(gulp.dest(outDir));
+  });
+  done();
+});
