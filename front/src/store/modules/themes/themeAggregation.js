465179506a0f4e0e6b2763864e04933c2971fe7e
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-16 14:06:41 -0800

    Edit lightTheme to lightBearTheme

--- front/src/store/modules/themes/themeAggregation.js
+++ front/src/store/modules/themes/themeAggregation.js
@@ -1,16 +1,16 @@
-import lightTheme from "@/store/modules/themes/lightTheme";
+import bareLightTheme from "@/store/modules/themes/bareLightTheme";
 
 const state = {
   // For now, add lightTheme in currentTheme. Do replacing, etc. later
   // When naming descriptions of themes, omit color, because it should be implied by default
 
   currentTheme: {
-    name: "Light Theme",
-    ...lightTheme
+    name: "Bare Theme",
+    ...bareLightTheme
   },
   lightTheme: {
-    name: "Light Theme",
-    ...lightTheme
+    name: "Bare Theme",
+    ...bareLightTheme
   },
   darkTheme: {},
   outlineTheme: {}
